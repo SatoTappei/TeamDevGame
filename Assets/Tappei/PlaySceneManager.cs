df20fa263e20ab2127f7f9a2a39573276c81645e
SatouTappei <stutpi.signup@gmail.com>
2022-11-10 02:04:03 +0900

    カードをマウスオーバーすると拡大して表示されるようにした

--- Assets/Tappei/PlaySceneManager.cs
+++ Assets/Tappei/PlaySceneManager.cs
@@ -1,19 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
 /// <summary>
 /// ゲームプレイの進行を制御する
 /// </summary>
 public class PlaySceneManager : MonoBehaviour
 {
-    void Start()
+    /// <summary>ゲームの勝敗が決まる勝利数</summary>
+    readonly int _gameSetPoint = 4;
+    /// <summary>決着がついたかを判定する</summary>
+    bool _isGameSet;
+
+    [SerializeField] PlayerUnit _player1;
+    [SerializeField] PlayerUnit _player2;
+
+    async void Start()
     {
-        Debug.Log("Hello World");
+        await StartStag();
+
+        _isGameSet = true; // テスト用、消してよし
+        while (!_isGameSet)
+        {
+            // 両者がカードを選ぶ
+            // カードをマウスオーバーしたら拡大して表示される
+            // クリックしたら場に出る
+            // 元々場にあったカードはスライドして戻る
+            // きちんと並び替える
+
+            // どちらもカードを出したら判定
+            // カードを減らす
+            // ゲームセットか判定
+            // ゲームセットならループを抜ける
+        }
+
+        // 結果表示の演出
+        // もう一度やるか選ぶ
     }
 
     void Update()
     {
         
+    }
+
+    /// <summary>ゲームスタートの演出</summary>
+    async UniTask StartStag()
+    {
+        // ここに演出の処理を書く
+        await UniTask.Delay(1000);
     }
 }
---
+++ Assets/Tappei/PlayerUnit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーの制御を行う
+/// </summary>
+public class PlayerUnit : MonoBehaviour
+{
+    /// <summary>現在の勝利数</summary>
+    int _winCount;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
